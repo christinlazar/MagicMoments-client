dc4808df6dd07c016382342da4dea40138ef8faa
christinlazar <christinlazar19@gmail.com>
2024-08-24 10:42:26 +0530

    chnaged name in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,7 @@ const clientid:any = process.env.REACT_APP_CLIENT_ID
 root.render(
   <Provider store={store}>
         <React.StrictMode>
-        <LoadScript googleMapsApiKey={`${process.env.REACT_APP_maps_key_2}`}  loadingElement={<LoadingComponent/>} >
+        <LoadScript googleMapsApiKey={`${process.env.REACT_APP_MAPS_API_KEY}`}  loadingElement={<LoadingComponent/>} >
   <SocketContextProvider>
   <GoogleOAuthProvider clientId={clientid}>
              <App />
