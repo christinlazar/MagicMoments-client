22fe22ae1001a3608c143404aaf3fc9a894bf0f1
christinlazar <christinlazar19@gmail.com>
2024-08-26 02:51:27 +0530

    added same domain

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -2,7 +2,7 @@ import axios,{AxiosInstance} from 'axios'
 
 
 const Api:AxiosInstance = axios.create({
-    baseURL:'http://localhost:5000/api',
+    baseURL:'https://magic-moments-client.vercel.app//api',
     withCredentials:true
 }) 
 
