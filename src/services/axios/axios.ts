ce604a1cd2d54d033afab38c4dd31a18f24e6931
christinlazar <christinlazar19@gmail.com>
2024-08-26 02:10:25 +0530

    back to normal baseURL in frontend

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -2,7 +2,7 @@ import axios,{AxiosInstance} from 'axios'
 
 
 const Api:AxiosInstance = axios.create({
-    baseURL:'https://43.205.227.83:5000/api',
+    baseURL:'http://localhost:5000/api',
     withCredentials:true
 }) 
 
