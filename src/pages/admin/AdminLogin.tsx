2dd82407d5d454487ada70c928f1b3a55d2cab0a
christinlazar <christinlazar19@gmail.com>
2024-09-02 10:12:32 +0530

    updation:Logo and fontstyles changed in adminLogin

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
-import logo from '../../assets/license (1).png'
+import logo from '../../assets/wedding (2).png'
 import RegisterImage from '../../assets/samantha-gades-x40Q9jrEVT0-unsplash.jpg'
 import { Toaster ,toast } from 'sonner'
 import { adminLogin } from '../../api/adminApi'
@@ -46,27 +46,27 @@ function AdminLogin() {
       <div className="text-white">
         <div className="mb-2 flex flex-col items-center">
           <img className='my-4' src={logo} width="50" alt="Magic Moments Logo" />
-          <h1 className="mb-2 text-xl font-serif text-cyan-950 font-bold">Magic Moments</h1>
+          <h1 className="mb-2 text-xl font-montserrat text-cyan-950 font-bold">Magic Moments</h1>
         </div>
         <form onSubmit={handleSubmit} id='form'>
-          <label className='font-serif text-cyan-950 text-xs'>Email</label>
+          <label className='font-montserrat text-cyan-950 text-xs'>Email</label>
           <div className="mb-4 text-lg">
             <input
 
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="bg-white bg-opacity-10 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none sm:text-sm"
+              className="bg-white bg-opacity-10 text-xs placeholder:italic placeholder:text-slate-400 block w-full border text-cyan-950 border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none sm:text-sm"
               type="text"
               name="email"
             />
           </div>
-          <label className='font-serif text-cyan-950 text-xs'>Password</label>
+          <label className='font-montserrat text-cyan-950 text-xs'>Password</label>
           <div className="mb-4 text-lg">
             <input
   
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="bg-white bg-opacity-10 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none sm:text-sm"
+              className="bg-white bg-opacity-10 placeholder:italic placeholder:text-slate-400 block w-full border text-xs  border-slate-300 rounded-md py-2 pl-3 pr-3 text-cyan-950 shadow-sm focus:outline-none sm:text-sm"
               type="password"
               name="password"
             />
@@ -74,7 +74,7 @@ function AdminLogin() {
           <div className="mt-8 flex justify-center text-lg text-black">
             <button
               type="submit"
-              className="rounded-3xl bg-cyan-950 bg-opacity-100 px-10 py-2 mb-10 text-white shadow-xl backdrop-blur-md transition-colors duration-300 transform hover:scale-105 hover:transition ease-out duration-300 font-serif"
+              className="rounded-3xl bg-cyan-950 bg-opacity-100 px-10 py-2 mb-10 text-sm text-white shadow-xl backdrop-blur-md transition-colors  transform hover:scale-105 hover:transition ease-out duration-300 font-montserrat"
             >
               Sign In
             </button>
