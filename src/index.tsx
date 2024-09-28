c11cda0d971abefe631fdc85d283716bc5ea5cce
christinlazar <christinlazar19@gmail.com>
2024-09-28 12:34:09 +0530

    updation:space added

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
 const clientid:any = process.env.REACT_APP_CLIENT_ID
 root.render(
   <Provider store={store}>
