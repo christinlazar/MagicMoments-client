884329bd8e813868b6a332a00dd18f756134221e
christinlazar <christinlazar19@gmail.com>
2024-10-03 18:22:42 +0530

    updation:changed frontend api

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios,{AxiosInstance} from 'axios'
 import { Role } from '../../interfaces/TypesAndInterfaces';
 const Api:AxiosInstance = axios.create({
-    baseURL:'https://adorehome.site/api',
+    baseURL:'https://genex.site/api',
     withCredentials:true
 }) 
 localStorage.debug = '*';
